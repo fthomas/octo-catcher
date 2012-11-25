efba22942e98ac2bb0b4a45b0983921a166f7f4f
Frank S. Thomas <frank@timepit.eu>
2012-11-25 11:00:44 +0100

    Added more API calls.

--- src/main/scala/octocatcher/gpodder/API.scala
+++ src/main/scala/octocatcher/gpodder/API.scala
@@ -1,7 +1,7 @@
 package octocatcher.gpodder
 
 import octocatcher._
-import octocatcher.util._
+import octocatcher.util.JSONOps._
 
 import scala.concurrent.Future
 import spray.client.HttpConduit
@@ -11,28 +11,76 @@ import spray.util._
 
 object test extends App {
 
-  val response = API.getToplist(2)
-
+  //val response = API.getToplist(2)
+  val response = API.getPodcastData("http://feeds.feedburner.com/linuxoutlaws")
+  
   response onSuccess {
     case x => println(x)
   }
 }
 
 object API {
+  /** Returns the count most used tags.
+    *
+    * gpodder.net API Reference:
+    * [[http://wiki.gpodder.org/wiki/Web_Services/API_2/Directory#Retrieve_Top_Tags Retrieve Top Tags]]
+    */
   def getTopTags(count: Int): Future[List[Any]] = {
     require(count >= 0)
     getJSONArray(s"/api/2/tags/${count}.json")
   }
 
+  /** Returns the count most-subscribed podcasts that are tagged with tag. 
+    *
+    * gpodder.net API Reference:
+    * [[http://wiki.gpodder.org/wiki/Web_Services/API_2/Directory#Retrieve_Podcasts_for_Tag Retrieve Podcasts for Tag]]
+    */
+  def getPodcastsForTag(tag: String, count: Int): Future[List[Any]] = {
+    require(tag.length > 0 && count >= 0)
+    getJSONArray(s"/api/2/tag/${tag}/${count}.json")
+  }
+
+  /** Returns information for the podcast with the given URL.
+    *
+    * gpodder.net API Reference:
+    * [[http://wiki.gpodder.org/wiki/Web_Services/API_2/Directory#Retrieve_Podcast_Data Retrieve Podcast Data]]
+    */
+  def getPodcastData(url: String): Future[Map[String, Any]] = {
+    require(url.length > 0)
+    getJSONObject(s"/api/2/data/podcast.json?url=${url}")
+  }
+
+  /** ???
+    *
+    * gpodder.net API Reference:
+    * [[http://wiki.gpodder.org/wiki/Web_Services/API_2/Directory#Retrieve_Episode_Data Retrieve Episode Data]]
+    */
+  def getEpisodeData(podcastUrl: String, episodeUrl: String) = {
+    require(podcastUrl.length > 0 && episodeUrl.length > 0)
+    getJSONObject(
+      s"/api/2/data/episode.json?podcast=${podcastUrl}&url=${episodeUrl}")
+  }
+
+  /** ???
+    *
+    * gpodder.net API Reference:
+    * [[http://wiki.gpodder.org/wiki/Web_Services/API_2/Directory#Podcast_Toplist Podcast Toplist]]
+    */
   def getToplist(number: Int): Future[List[Any]] = {
     require(1 to 100 contains number)
     getJSONArray(s"/toplist/${number}.json")
   }
 
-  private def getJSONArray(uri: String): Future[List[Any]] = {
+  private def requestUri(uri: String): Future[HttpResponse] = {
     val pipeline = HttpConduit.sendReceive(httpConduit())
-    val response = pipeline(HttpRequest(GET, uri))
+    pipeline(HttpRequest(GET, uri))
+  }
 
-    response.map(JSONOps.toJSONArray(_))
+  private def getJSONArray(uri: String): Future[List[Any]] = {
+    requestUri(uri).map(toJSONArray(_))
+  }
+
+  private def getJSONObject(uri: String): Future[Map[String, Any]] = {
+    requestUri(uri).map(toJSONObject(_))
   }
 }
