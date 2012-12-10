5a9ed9c03aaf958230683085a2b06505b3bf9efd
Frank S. Thomas <frank@timepit.eu>
2012-12-10 22:30:08 +0100

    Update for RC3.

--- src/main/scala/octocatcher/package.scala
+++ src/main/scala/octocatcher/package.scala
@@ -15,18 +15,15 @@
 import akka.actor.{ActorSystem, Props}
 import com.typesafe.config.ConfigFactory
 import spray.can.client.HttpClient
-import spray.io.IOBridge
+import spray.io.IOExtension
 
 package object octocatcher {
   implicit val actorSystem = ActorSystem("octo-catcher")
 
-  val ioBridge = new IOBridge(actorSystem).start()
+  val ioBridge = IOExtension(actorSystem).ioBridge
+
   val httpClient = {
     val cfg = ConfigFactory.parseString("spray.can.client.ssl-encryption = on")
     actorSystem.actorOf(Props(new HttpClient(ioBridge, cfg)))
-  }
-
-  actorSystem.registerOnTermination {
-    ioBridge.stop()
   }
 }
